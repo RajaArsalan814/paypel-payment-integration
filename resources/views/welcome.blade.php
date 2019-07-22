a8a05c1147dc3c55c1718c7d8bf69dd65bc8b730
RajaArsalan814 <rarsalan0@gmail.com>
2019-07-22 13:04:46 -0700

    Payment integration paypal

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<div id="paypal-button"></div>
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
+<script>
+  paypal.Button.render({
+    // Configure environment
+    env: 'sandbox',
+    client: {
+      sandbox: 'Aeh1KllgX5GkOzLl7rLWJxBguefV3k8NPZDJ9UDFwbcr9U1WuVS-m4B-_VCfiG5mUCtjCJBCJsjB45so',
+      production: 'demo_production_client_id'
+    },
+    // Customize button (optional)
+    locale: 'en_US',
+    style: {
+      size: 'small',
+      color: 'blue',
+      shape: 'pill',
+    },
+
+    // Enable Pay Now checkout flow (optional)
+    commit: true,
+
+    // Set up a payment
+    payment: function(data, actions) {
+      return actions.payment.create({
+        transactions: [{
+          amount: {
+            total: '20.01',
+            currency: 'USD'
+          }
+        }]
+      });
+    },
+    // Execute the payment
+    onAuthorize: function(data, actions) {
+      return actions.payment.execute().then(function() {
+        // Show a confirmation message to the buyer
+        window.alert('Thank you for your purchase!');
+      });
+    }
+  }, '#paypal-button');
+
+</script>
